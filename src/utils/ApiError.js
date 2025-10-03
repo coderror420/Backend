9741847eb170a760eb31e1c07befb61ff16acd7f
Kanishka <kanishkap2308@gmail.com>
2025-10-03 12:29:22 +0530

    setup files

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -3,7 +3,7 @@ class ApiError extends Error {
        statusCode,
        message="Something went wrong",
         errors = [],
-        statck=""
+        stack=""
 
         
     ){
@@ -12,9 +12,9 @@ class ApiError extends Error {
         this.data=null,
         this.message=message,
         this.success=false,
-        this.errors=this.errors  
-        if(statck){
-            this.stack=statck
+        this.errors=errors  
+        if(stack){
+            this.stack=stack
         } else{
             Error.captureStackTrace(this,this.constructor)
         }
